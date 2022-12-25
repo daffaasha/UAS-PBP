a041990568b32ba84bbc49914484e981c8f03996
daffaasha <aadaffa41@gmail.com>
2022-12-26 06:52:45 +0700

    making home

--- front/home.php
+++ front/home.php
@@ -7,132 +7,46 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Home</title>
   <link rel="shortcut icon" href="../res/meong img.jpg" type="image/x-icon" />
-  <script src="https://kit.fontawesome.com/afa3537b19.js" crossorigin="anonymous"></script>
-  <!-- CSS only -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
-  <link rel="stylesheet" href="css/home.css" />
-  <!-- JavaScript Bundle with Popper -->
+  <link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg nav-prop">
-
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">
-        <img src="../res/meong img.jpg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top brand-logo" />
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup"></div>
-    </div>
-    <ul class="navbar-nav me-auto">
-      <li class="nav-item">
-        <a href="#" class="nav-link"><i class="fa-solid fa-clock-rotate-left icon-prop fa-lg"></i></a>
-      </li>
-      <li class="nav-item">
-        <a href="#" class="nav-link"><i class="fa-regular fa-circle-user icon-prop fa-lg"></i></a>
-      </li>
-    </ul>
-  </nav>
-
-  <div class="main">
-    <div class="row search-container">
-      <div class="input-group mb-3 no-margin">
-        <span class="input-group-text login-input login-span" id="basic-addon1"><i class="fa-solid fa-magnifying-glass"></i>
-        </span>
-        <input type="text" class="form-control input-container shadow-none border-start-0 login-input form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" />
+  <header>
+    <nav>
+      <div class="logo">
+        <a href="home.php" target="_blank" rel="noopener noreferrer"><img src="img/petstar.png" alt="" width="50%"></a>
       </div>
-    </div>
+      <div class="menu">
+        <a href="#"><img src="img/history.svg" alt="nama-user" width="75%"></a>
+        <a href="profile.php"><img src="img/user-white.svg" alt="setting" width="75%"></a>
+    </nav>
+  </header>
 
-    <div class="row">
-      <div class="col">
-        <h4 class="product">Products</h4>
+  <main>
+    <div class="product-side">
+      <div class="search-container">
+        <img src="img/lup.svg" alt="find" class="find-logo">
+        <input type="text" class="search-input">
       </div>
-      <div class="col">
-        <button type="button" class="btn btn-primary btn-tambah">
-          + Tambah
-        </button>
+      <div class="title-container">
+        <p class="product-title">Product</p>
+        <input type="button" value="+ Add" class="btn-tambah">
       </div>
-    </div>
-
-    <div class="container-fluid no-margin">
-      <div class="row g-0">
-        <div class="col col-card">
-          <input class="form-check-input justify-content-center" type="checkbox" value="" id="product-checkboxes" />
-          <label class="form-check-label" for="flexCheckDefault">
-            <div class="card">
-              <img src="../res/meong img.jpg" class="card-img-top" alt="product" />
-              <div class="card-img-overlay text-end card-content">
-                <a href="#"><i class="fa-solid fa-pen-to-square"></i></a>
-              </div>
-              <div class="card-body card-content">
-                <p class="card-text">Ini Judul Produk</p>
-                <h5 class="card-title">Rp 60.000</h5>
-              </div>
-              <div class="card-body text-end card-content">
-                <span class="stok"> </span>
-              </div>
-            </div>
-          </label>
-        </div>
-
-      </div>
-      <div class="row g-0">
-        <div class="col col-card">
-          <input class="form-check-input justify-content-center" type="checkbox" value="" id="product-checkboxes" />
-          <label class="form-check-label" for="flexCheckDefault">
-            <div class="card">
-              <img src="../res/meong img.jpg" class="card-img-top" alt="product" />
-              <div class="card-img-overlay text-end card-content">
-                <a href="#"><i class="fa-solid fa-pen-to-square"></i></a>
-              </div>
-              <div class="card-body card-content">
-                <p class="card-text">Ini Judul Produk</p>
-                <h5 class="card-title">Rp 60.000</h5>
-              </div>
-              <div class="card-body text-end card-content">
-                <span class="stok"> </span>
-              </div>
-            </div>
-          </label>
-        </div>
-        <div class="row g-0">
-          <div class="col col-card">
-            <input class="form-check-input justify-content-center" type="checkbox" value="" id="product-checkboxes" />
-            <label class="form-check-label" for="flexCheckDefault">
-              <div class="card">
-                <img src="../res/meong img.jpg" class="card-img-top" alt="product" />
-                <div class="card-img-overlay text-end card-content">
-                  <a href="#"><i class="fa-solid fa-pen-to-square"></i></a>
-                </div>
-                <div class="card-body card-content">
-                  <p class="card-text">Ini Judul Produk</p>
-                  <h5 class="card-title">Rp 60.000</h5>
-                </div>
-                <div class="card-body text-end card-content">
-                  <span class="stok"> </span>
-                </div>
-              </div>
-            </label>
+      <div class="product-container">
+        <div class="product-card">
+          <img src="img/meong-img.jpg" alt="product" class="img-product">
+          <div class="product-info">
+            <p class="product-name">Ini Nama Product</p>
+            <p class="product-price">60.000</p>
           </div>
-
+          <input type="button" value="Add Product" class="product-add">
         </div>
-
       </div>
     </div>
+    <div class="price-side">
 
-  </div>
-
-  <footer>
-    <div class="submit-container">
-      <input type="button" value="LANJUT" style="float: right;" class="btn-submit">
     </div>
-  </footer>
-
-  <form action="#" method="get"></form>
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </main>
 </body>
 
 </html>
