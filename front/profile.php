8e5c44511c01fb800288c208fb05cee13f99d44d
daffaasha <aadaffa41@gmail.com>
2022-12-25 14:07:11 +0700

    finished profile

--- front/profile.php
+++ front/profile.php
@@ -20,13 +20,66 @@
         <a href="#"><img src="img/user-white.svg" alt="setting" width="75%"></a>
     </nav>
   </header>
+
   <main>
-    <h2 class="profile-title"></h2>
-    <div class="profile-data"></div>
-    <h2 class="password-title"></h2>
-    <div class="password data"></div>
-    <input type="text" class="save-data">
-    <input type="text" class="logout">
+    <div class="title-container">
+      <img src="img/left-arrow.svg" alt="back" width="24px">
+      <p class="profile-title">Profile</p>
+    </div>
+
+    <form action="#" method="post">
+      <div class="profile-data">
+        <div class="profile-pict-container">
+          <img src="img/meong img.jpg" alt="profile" class="profile-pict">
+        </div>
+        <div class="profile-form">
+          <div class="form-flex">
+            <p class="label">Username</p>
+            <div class="input-form-container">
+              <input type="text" class="input-form">
+              <img src="img/pen.svg" alt="edit" class="edit-icon">
+            </div>
+          </div>
+          <div class="name form-flex">
+            <p class="label">Name</p>
+            <div class="input-form-container">
+              <input type="text" class="input-form">
+              <img src="img/pen.svg" alt="edit" class="edit-icon">
+            </div>
+          </div>
+          <div class="email form-flex">
+            <p class="label">Email</p>
+            <div class="input-form-container">
+              <input type="text" class="input-form">
+              <img src="img/pen.svg" alt="edit" class="edit-icon">
+            </div>
+          </div>
+          <div class="number form-flex">
+            <p class="label">Number</p>
+            <div class="input-form-container">
+              <input type="text" class="input-form">
+              <img src="img/pen.svg" alt="edit" class="edit-icon">
+            </div>
+          </div>
+        </div> 
+      </div>
+
+      <h2 class="account-title profile-title">Password</h2>
+
+      <div class="account-data">
+        <div class="form-flex">
+          <p class="password label">Password</p>
+          <div class="input-form-container">
+            <input type="text" class="input-form">
+            <img src="img/pen.svg" alt="edit" class="edit-icon">
+          </div>
+        </div>
+      </div>
+
+      <input type="button" value="SIMPAN" class="save-data">
+    </form>
+    
+    <input type="button" value="LOG OUT" class="logout">
   </main>
 </body>
 
